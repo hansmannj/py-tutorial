08f2c53f91a3a2163e6da8688a3540a5e7c23163
eraymann <elias.raymann@gmail.com>
2019-08-12 12:04:03 +0200

    solution summe

---
+++ solutions/summe.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+liste = [1, 2, 3, 4, 5, 6]
+
+# Solution 1: for-loop
+summe = 0
+for zahl in liste:
+    summe += zahl
+print(summe)
+
+# Solution 2: built-in function
+print(sum(liste))
