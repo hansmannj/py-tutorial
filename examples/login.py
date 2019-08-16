7274ab86c70d0bf65a9113ede733579721972c31
Elias Raymann <elias.raymann@gmail.com>
2019-08-16 09:25:18 +0200

    login example

--- examples/login.py
+++ examples/login.py
@@ -1,16 +1,12 @@
-import hashlib
-import json
-import tkinter.messagebox
 from tkinter import *
 
 
-class Grid:
+class Login:
 
     def __init__(self):
         self.window = Tk()
 
         self.window.title("Login")
-        
         self.window.bind(sequence="<Escape>", func=self.cancel)
         self.window.bind(sequence="<Return>", func=self.login)
 
@@ -30,19 +26,14 @@ class Grid:
         pwd = Entry(master=frame, width=30, show="*")
         pwd.grid(row=1, column=1, padx=10, pady=10)
 
-        self.btn_cancel = Button(master=self.window, text="Cancel", width=10)
-        self.btn_cancel.bind(sequence="<Button-1>", func=self.cancel)
-        self.btn_cancel.pack(padx=10, pady=10, side=LEFT)
-        
+        btn_cancel = Button(master=self.window, text="Cancel", width=10)
+        btn_cancel.bind(sequence="<Button-1>", func=self.cancel)
+        btn_cancel.pack(padx=10, pady=10, side=LEFT)
 
-        self.btn_login = Button(master=self.window, text="Login", width=10, command=self.login)
-        self.btn_login.pack(padx=10, pady=10, side=RIGHT)
+        btn_login = Button(master=self.window, text="Login", width=10, command=self.login)
+        btn_login.pack(padx=10, pady=10, side=RIGHT)
 
-        
-        
-
-        self.btn_login.bind(sequence="<Button-1>", func=self.login)
-        
+        btn_login.bind(sequence="<Button-1>", func=self.login)
 
         self.window.resizable(width=False, height=False)
         self.window.mainloop()
@@ -50,39 +41,8 @@ class Grid:
     def cancel(self, event):
         self.window.destroy()
 
-
     def login(self, event):
-        self.btn_login.focus()
-        username = self.user.get()
-        password = hashlib.md5(self.pwd.get().encode("utf-8")).hexdigest()
-
-        # existierende Benutzer laden
-        users = self.load_users()
-
-        if (username, password) in users.items():
-            tkinter.messagebox.showinfo(title="Login", message="Erfolgreich eingelogged")
-            self.window.destroy()
-        elif username in users.keys():
-            result = tkinter.messagebox.askretrycancel(title="Passwort falsch", message="Ungültiges Passwort")
-            if result:
-                self.pwd.config(text="")
-                self.pwd.focus()
-            else:
-                self.window.destroy()
-        else:
-            result = tkinter.messagebox.askretrycancel(title="Unbekannter Benutzer", message="Benutzername unbekannt")
-            if result:
-                self.user.config(text="")
-                self.user.focus()
-                self.pwd.config(text="")
-            else:
-                self.window.destroy()
-
-    @staticmethod
-    def load_users():
-        with open(r"..\resources\users.json", "r") as r:
-            return json.loads(r.read())
+        pass
 
 
-# Programm ausführen
-Grid()
+Login()
