1fa5225d57c86f93d6c1a2b2ffb9f2f2b780e2ff
Juergen Hansmann <Juergen.Hansmann@swisstopo.ch>
2023-03-17 14:05:38 +0100

    weitere Lösungen hinzugefügt

---
+++ solutions/sehtest.py
@@ -0,0 +1,22 @@
+import random
+import string
+from tkinter import Label
+from tkinter import Tk
+
+
+class Sehtest:
+
+    def __init__(self):
+        window = Tk()
+        window.title("Sehtest")
+        # window.geometry("500x500")
+        for i in range(4):
+            buchstaben = random.choices(string.ascii_uppercase, k=3)
+            label = Label(
+                master=window, width=50, text=buchstaben, font=("Arial", int(100 - (i * 20)))
+            )
+            label.pack(padx=10, pady=10)
+        window.mainloop()
+
+
+Sehtest()
