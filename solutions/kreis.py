5ea13f3d03b842015ae5573087de70c8552195f8
Jürgen Hansmann <juergen.hansmann@swisstopo.ch>
2024-03-12 10:55:22 +0100

    Lösungen Kurstag 1

---
+++ solutions/kreis.py
@@ -0,0 +1,40 @@
+import math
+
+# Variante mit Parameter
+# Wird beim Starten des Programms von aussen mitgegeben
+# user_input = sys.argv[1]
+
+# Variante mit Benutzereingabe
+# Wird zur Laufzeit des Programms vom Benutzer verlangt
+while True:
+    radius = input("Bitte den Radius eingeben: ")
+    try:
+        # Benutzereingaben sind immer Strings
+        # Deshalb casting nach Fliesskommazahl
+        radius = float(radius)
+        if radius < 0:
+            raise Exception("Radius darf nicht negativ sein")
+        break
+
+    except ValueError:
+        print("Bitte einen Integer oder Float eingeben!")
+    except Exception as e:
+        print("Bitte Zahl grösser Null eingeben")
+        print(e)
+
+
+
+# Berechnungen
+diameter = radius * 2
+circumference = radius * 2 * math.pi
+area = round(radius**2 * math.pi, 3)
+
+# Ausgabe (vier verschiedene Möglichkeiten für print)
+print(f"Radius: {radius}")
+print(f"Durchmesser: {diameter}")
+print(f"Umfang: {str(circumference)}")
+# Den Flächeninhalt unten auf 2 Nachkommastellen gerundet, damit es lesbarer ist.
+# Auf zwei Möglichkeiten
+# (Könnte man natürlich für den Umfang oben auch machen.)
+print(f"Fläche: {round(area, 2)}")
+print(f"Fläche: {area:.2f}")
